57698da90a3c297ee35c24a6cf11594586d4fece
Joni Bekenstein <joni@bek.io>
2021-06-17 08:03:11 -0300

    Add second maze

--- src/mazes.ts
+++ src/mazes.ts
@@ -12,3 +12,18 @@ XX XX X   XX
 X   X   X  E
 XXXXXXXXXXXX
 `
+
+export const MAZE1 = `
+XXXXXXXXXXXX
+X X   X    E
+X   XXX X XX
+XXXXX   X  X
+X     XXXX X
+X XX XX    X
+X     X X XX
+XXX X X    X
+X   X XX XXX
+XX XX X   XX
+S       X  X
+XXXXXXXXXXXX
+`
